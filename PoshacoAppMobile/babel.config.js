cde6b02fa47f9fd7234435260b40ec99947a9780
SauLuoiBo2 <101506269+SauLuoiBo2@users.noreply.github.com>
2022-05-03 08:07:45 +0700

    dsf

--- PoshacoAppMobile/babel.config.js
+++ PoshacoAppMobile/babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset', '@babel/preset-flow'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['./'],
+                alias: {
+                    '@src': ['./src'],
+                    '@util': ['./src/util'],
+                    '@assets': ['./src/assets'],
+                    '@config': ['./src/config'],
+                    '@screen': ['./src/screen'],
+                    '@navigation': ['./src/navigation'],
+                    '@components': ['./src/components'],
+                    '@queries': ['./src/queries'],
+                    '@context': ['./src/context'],
+                    '@hooks': ['./src/hooks'],
+                },
+                extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.json'],
+            },
+        ],
+    ],
 };
---
+++ PoshacoAppMobile/src/screen/Ha.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Ha = () => {
+    return (
+        <View>
+            <Text>Ha</Text>
+        </View>
+    );
+};
+
+export default Ha;
